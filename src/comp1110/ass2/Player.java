229e9b38618c330f2b470f38643b27713a404812
Runyao Wang <u6812566@anu.edu.au>
2023-08-21 15:42:31 +1000

    create class draft

---
+++ src/comp1110/ass2/Player.java
@@ -0,0 +1,8 @@
+package comp1110.ass2;
+
+public class Player {
+    private char color;//"c""y""r""p"
+
+    private int dirhams;
+    private int rugs;
+}
---
+++ src/comp1110/ass2/Rug.java
@@ -0,0 +1,9 @@
+package comp1110.ass2;
+
+public class Rug {
+    private char color;
+    private int ID;
+    private int[] p1;
+    private int[] p2;
+    // check isCovered
+}
