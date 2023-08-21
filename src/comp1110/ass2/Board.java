6e90de2496dd6d441095d3c4515ffe439e9e1f56
Runyao Wang <u6812566@anu.edu.au>
2023-08-21 17:17:15 +1000

    Board draft

--- src/comp1110/ass2/Board.java
+++ src/comp1110/ass2/Board.java
@@ -1,5 +1,18 @@
 package comp1110.ass2;
 
 public class Board {
+    private char[][] squareColor = new char[7][7];
 
+    public Board()
+
+    //getter
+    public char getColor(int x, int y){
+        return squareColor[x][y];
+    }
+
+    //set color
+    public void setColor(int x, int y, char color)
+
+    //check how many same color rugs are connected with the given coordinate.
+    public int connectedRugs (int x, int y)
 }
