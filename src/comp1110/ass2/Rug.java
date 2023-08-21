85af3d8b44df5cc4b86fd5e3013f5cab5f980639
Runyao Wang <u6812566@anu.edu.au>
2023-08-21 16:45:30 +1000

    Rug deaft

--- src/comp1110/ass2/Rug.java
+++ src/comp1110/ass2/Rug.java
@@ -2,8 +2,15 @@ package comp1110.ass2;
 
 public class Rug {
     private char color;
-    private int ID;
-    private int[] p1;
-    private int[] p2;
-    // check isCovered
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+    
+    // Constructor
+    public Rug(int x1, int y1, int x2, int y2, char color)
+    
+    //getters
+
+    
 }
