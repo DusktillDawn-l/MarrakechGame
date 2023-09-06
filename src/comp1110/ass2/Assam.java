d1a47ee774d188da9e092d9692a42efe28e258cd
SunHeng <1296395592@qq.com>
2023-09-06 16:29:35 +1000

    complete assamString

--- src/comp1110/ass2/Assam.java
+++ src/comp1110/ass2/Assam.java
@@ -1,21 +1,28 @@
 package comp1110.ass2;
 
-enum Direction {
-        N,
-        E,
-        S,
-        W
-}
-
 public class Assam {
     private int x;
     private int y;
     Direction dir;
+    private String assamString;
+    private final int assamStringLength = 4;
+    public static final int assamDigit = 0;
+    public static final int xCoordinateDigit = 1;
+    public static final int yCoordinateDigit = 2;
+    public static final int orientationDigit = 3;
 
     public Assam(int x, int y, Direction dir) {
         this.x = x;
         this.y = y;
         this.dir = dir;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.setLength(assamStringLength);
+        stringBuilder.setCharAt(assamDigit,'A');
+        stringBuilder.setCharAt(xCoordinateDigit,Character.forDigit(x,10));
+        stringBuilder.setCharAt(yCoordinateDigit,Character.forDigit(y,10));
+        String direction = dir.toString();
+        stringBuilder.setCharAt(orientationDigit,direction.charAt(0));
+        assamString = stringBuilder.toString();
     }
 
 
@@ -37,5 +44,9 @@ public class Assam {
 
     public void move(int steps){
         return;
+    }
+
+    public String getAssamString(){
+        return assamString;
     }
 }
